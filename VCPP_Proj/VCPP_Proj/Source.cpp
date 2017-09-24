5a2b88e5ff657d6e4f5b89644e0069dfec86f792
PraveenMax <com.java.praveen@gmail.com>
2017-09-24 12:34:20 +0530

    Code cleanup. Moved the GLFW and GLAD initializations into a separate function. Render logic is more readable.

--- VCPP_Proj/VCPP_Proj/Source.cpp
+++ VCPP_Proj/VCPP_Proj/Source.cpp
@@ -1,21 +1,48 @@
-
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
 
-// settings
+GLFWwindow * initializeGLFWwindow(const unsigned int SCREEN_WIDTH, const unsigned int SCREEN_HEIGHT);
+
+//DISPLAY VARS
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
+	GLFWwindow* window = initializeGLFWwindow(SCR_WIDTH, SCR_HEIGHT);
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+
+		glClearColor(0.5f,0.5f,0.5f,0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	glfwTerminate();
+
+	cout << "Closing GLFW . Bye!"<< endl;
+	return 0;
+}
+
+/*
+	This function creates and assigns a new GLFW Window to the current context.
+	Also, loads the OPENGL functions via GLAD
+*/
+GLFWwindow * initializeGLFWwindow(const unsigned int SCREEN_WIDTH, const unsigned int SCREEN_HEIGHT)
+{
 	// glfw: initialize and configure
 	if (!glfwInit())
 	{
-		cout << "Failed to initialize GLFW. Terminating now!" << endl;
-		return -1;
+		cout << "** ERROR : Failed to initialize GLFW. Terminating now!" << endl;
+		return nullptr;
 	}
 	else
 	{
@@ -25,45 +52,25 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	
+
 	// glfw window creation
-	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Core OpenGL", NULL, NULL);
 	if (window == NULL)
 	{
-		cout << "Failed to create GLFW window" << endl;
+		cout << "** ERROR : Failed to create GLFW window. Terminating now!" << endl;
 		glfwTerminate();
-		return -1;
+		return nullptr;
 	}
+
+	//Set this window's context to the calling thread 
 	glfwMakeContextCurrent(window);
 
 	// glad: load all OpenGL function pointers
-	// ---------------------------------------
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		cout << "Failed to initialize GLAD.Unable to find func pointers for GL functions!" << endl;
-		return -1;
+		return nullptr;
 	}
 
-	// render loop
-	while (!glfwWindowShouldClose(window))
-	{
-
-		// render
-		// ------
-		glClearColor(0.5f,0.5f,0.5f,0);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-
-	glfwTerminate();
-
-	cout << "Closing GLFW . Bye!"<< endl;
-	return 0;
+	return window;
 }
